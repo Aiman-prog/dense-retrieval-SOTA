318ee8aa8600dd20e727db904c2d48508c4037ba
Aiman <aimanaw50@proton.me>
2026-06-01 23:15:42 +0200

    revert to prod: gpu-a100, batch_size=64, mc_batch_size=1024

--- scripts/run_grass_singularity.sh
+++ scripts/run_grass_singularity.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name=grass
-#SBATCH --partition=gpu-a100-small
-#SBATCH --time=01:00:00
+#SBATCH --partition=gpu-a100
+#SBATCH --time=07:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=16
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=8000M
 #SBATCH --account=Education-EEMCS-MSc-DSAIT
